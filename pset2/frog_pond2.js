b2f662e9b6d940caf82d667410fa27c7f97d30d1
Tiger0320 <ywang64@berklee.edu>
2026-09-22 00:51:19 -0400

    commit

--- pset2/frog_pond2.js
+++ pset2/frog_pond2.js
@@ -1,10 +1,5 @@
 const activities = ["babysit tadpoles", "flies for lunch", "tongue stretch", "swimming lesson"];
 
-function getFrogActivity(input) {
-  const index = Number(input);
-  
-  // The modulo operator (%) wraps any integer back into array bounds [0 to length - 1]
-  const wrappedIndex = ((index % activities.length) + activities.length) % activities.length;
-  
-  return activities[wrappedIndex];
-}
+let index = prompt("choose activity");
+index = index % 4;
+print(activities[index]);
